019185974f1a62297a52c4f5feeecafa433eb04d
Lucas Gabriel <lucaskobielski@gmail.com>
2024-10-31 12:24:02 -0300

    LançamentoIphone

---
+++ src/Iphone.java
@@ -0,0 +1,20 @@
+public class iPhone {
+    private ReprodutorMusical reprodutorMusical;
+    private AparelhoTelefonico telefone;
+    private Navegador navegador;
+
+    public iPhone() {
+        this.reprodutorMusical = new ReprodutorMusical();
+        this.telefone = new AparelhoTelefonico();
+        this.navegador = new Navegador();
+    }
+
+    public void usarReprodutor() {
+    }
+
+    public void usarTelefone() {
+    }
+
+    public void usarNavegador() {
+    }
+}
